78dcf70e5e5efd6c5c7a0ecdc05b1a05d6d51176
annalhq <155549764+annalhq@users.noreply.github.com>
2025-05-19 22:41:00 +0530

    chore: minor fixes

--- day-1/hello.cu
+++ day-1/hello.cu
@@ -1,12 +1,13 @@
 #include <iostream>
 
 // The __global__ keyword marks a function to run on the GPU device
-__global__ void kernel(){}
+__global__ void kernel(){
+     printf("From the GPU\n");
+}
 
 // The <<<...>>> syntax specifies grid and block configuration for the kernel
 int main()
 {
-     kernel<<<1,1>>>();
-     printf("Hello, World!");
+     kernel<<<1,10>>>();
      return 0;
 }
