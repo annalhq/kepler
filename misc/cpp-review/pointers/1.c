e6c0db016117db5b5054204bb51617d63b36a97b
annalhq <annalhqshaikh@gmail.com>
2025-05-19 16:09:28 +0530

    misc: cpp review

---
+++ misc/cpp-review/pointers/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num = 10;
+    float fnum = 3.1415926;
+    void* vptr;
+
+    vptr = &num;
+
+    printf("integer: %d\n", *(int*)vptr);
+
+    vptr = &fnum;
+    printf("float integer: %.7f\n", *(float*)vptr);
+}
+// void pointers are used when we don't know the data type of the memory address
+// NOTE: malloc() returns a void pointer but we see it as a pointer to a specific data type after the cast (int*)malloc(4) or (float*)malloc(4) etc.
