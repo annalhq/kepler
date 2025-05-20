0ab14a8b6aa269add7e1e524304a914df47ee3ef
annalhq <155549764+annalhq@users.noreply.github.com>
2025-05-20 20:57:49 +0530

    feat: passing parameter

---
+++ basics/parameter.cu
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <../common/book.h>
+
+__global__ void kernel(int *a, int *b, int *c) {
+    *c = a+b;
+}
+
+int main(void){
+     int c;
+     int *dev_c;
+     HANDLE_ERROR(cudaMalloc((void**)&dev_c, sizeof(int)));
+
+     add<<<1,1>>>(2, 1, dev_c);
+
+     HANDLE_ERROR(cudaMemcpy(&c, dev_c, sizeof(int), cudaMemcpyDeviceToHost));
+
+     printf("2 + 1 = %d\n", c);
+
+     HANDLE_ERROR(cudaFree(dev_c));
+     
+     return 0;
+}
